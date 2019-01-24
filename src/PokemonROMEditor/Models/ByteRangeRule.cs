360bd9923337ddea4afe97d1b97d06833fd3b9fc
Jake Fordyce <JakeFordyce78@gmail.com>
2019-01-24 11:49:49 -0500

    Added some very basic validation

---
+++ src/PokemonROMEditor/Models/ByteRangeRule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PokemonROMEditor.Models
+{
+    public class ByteRangeRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            int byteValue = 0;
+
+            try
+            {
+                if (((string)value).Length > 0)
+                    byteValue = Int32.Parse((String)value);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, "Illegal characters or " + e.Message);
+            }
+
+            if ((byteValue < 1) || (byteValue > 255))
+            {
+                return new ValidationResult(false,
+                  "Please enter a value in the range: " + 1 + " - " + 255 + ".");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
